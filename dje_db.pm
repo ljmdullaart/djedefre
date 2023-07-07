28819459afebfd1c1538384847276a791d4fe256
ljmdullaart <ljm@acm.org>
2023-07-07 11:52:24 +0200

    Improved error handling.

--- dje_db.pm
+++ dje_db.pm
@@ -14,25 +14,37 @@ use DBI;
 
 my $db;
 my $db_sth;
+my $db_error=0;
 
 sub connect_db {
 	(my $dbfile)=@_;
 	$db = DBI->connect("dbi:SQLite:dbname=".$dbfile)
 		or die $DBI::errstr;
+	$db_error=0;
 	return $db;
 }
 
 
 sub db_dosql{
 	(my $sql)=@_;
-        $db_sth = $db->prepare($sql);
-        $db_sth->execute();
-	return $db_sth;
+        if ($db_sth = $db->prepare($sql)){
+        	$db_sth->execute();
+		$db_error=0;
+		return 0;
+	}
+	else { 
+		print "Prepare failed for $sql\n";
+		$db_error=1;
+		return 1;
+	}
 }
 
 sub db_getrow {
 	my @row;
-	if (@row  = $db_sth->fetchrow()){
+	if ($db_error==1){
+		return ();
+	}
+	elsif (@row  = $db_sth->fetchrow()){
 		return @row;
 	}
 	else {
