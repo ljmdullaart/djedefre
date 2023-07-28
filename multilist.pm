c75d52f6364a0b1f36d29223b3d881ecfad935ed
ljmdullaart <ljm@acm.org>
2023-07-28 16:11:25 +0200

    buttons for sorting by column.

--- multilist.pm
+++ multilist.pm
@@ -13,7 +13,7 @@ use File::Slurp;
 use File::Slurper qw/ read_text /;
 use File::HomeDir;
 use Data::Dumper;
-
+use Sort::Naturally qw/ncmp/;
 
 #	A single multilist is supported at one time.
 #	For a new multilist, do the following:
@@ -31,11 +31,16 @@ my $mlframe;
 	my $mlboxframe;
 
 my $mlheight;
+my $mlside;
 my $qcol;
 my @colwidth;
 my @colhead;
+my @mlcolval;
 my @mlcolumns;
+my $mlmaxcolval=0;
 my $scroll;
+my $mlparent;
+my $mlfirst=0;
 
 
 sub scroll_listboxes {
@@ -49,9 +54,15 @@ sub scroll_listboxes {
 
 sub ml_new {
 	(my $parent,my $height,my $side)=@_;
-	$mlframe->destroy if Tk::Exists($mlframe);
-	$mlframe=$parent->Frame(-height=>$height)->pack(-side=>$side);
-	$mlheight=$height*0.5;
+	$mlparent=$parent;
+	$mlheight=$height;
+	$mlside=$side;
+	$mlmaxcolval=0;
+	$mlfirst=0;
+	splice @colwidth;
+	splice @colhead;
+	splice @mlcolval;
+	splice @mlcolumns;
 }
 	
 
@@ -76,13 +87,37 @@ sub ml_colhead {
 	}
 }
 
+sub ml_insert {
+	(my @args)=@_;
+	for my $i (0 .. $#args){
+		if ($i<=$qcol){
+			$mlcolval[$i][$mlmaxcolval]=$args[$i]
+		}
+	}
+	$mlmaxcolval++;
+}
+
 sub ml_create {
+	$mlframe->destroy if Tk::Exists($mlframe);
+	$mlframe=$mlparent->Frame(-height=>$mlheight)->pack(-side=>$mlside);
 	$mlheaderframe=$mlframe->Frame()->pack(-side=>'top');
 	my $col;
+	if ($mlfirst==0){
+		ml_sortcols(0);
+		$mlfirst++;
+	}
 	for my $i (0 .. $qcol ){
 		$colwidth[$i]=10 unless defined $colwidth[$i];
 		$colwidth[$i]=10 if ($colwidth[$i] eq '');
-		$mlheaderframe->Label(-text=>$colhead[$i],-width=>$colwidth[$i], -font=>"arial 14")->pack(-side=>'left');
+		$mlheaderframe->Button(
+			-text=>$colhead[$i],
+			-width=>$colwidth[$i]-2, 
+			-font=>"arial 14",
+			-command => sub {
+				ml_sortcols($i);
+				ml_create();
+			}
+		)->pack(-side=>'left');
 	}
 	$mlboxframe=$mlframe->Frame()->pack(-side=>'bottom');
 	$scroll = $mlboxframe->Scrollbar( );
@@ -104,17 +139,44 @@ sub ml_create {
 	foreach my $list (@mlcolumns){
 		$list->pack(-side => 'left');
 	}
-}
-
-sub ml_insert {
-	(my @args)=@_;
-	for my $i (0 .. $#args){
-		$mlcolumns[$i]->insert('end',$args[$i]);
+	for (my $i=0; $i<$mlmaxcolval; $i++){
+		if (exists($mlcolval[$i])){
+			my @args=@{$mlcolval[$i]};
+			for my $j (0.. $#args){
+				if (defined($args[$j])){
+					$mlcolumns[$i]->insert('end',$args[$j]);
+				}
+			}
+		}
 	}
 }
 
 	
+sub ml_sortcols {
+	(my $col)=@_;
+	# Transpose the array
+	my @transposed ;
+	for my $row (0..@mlcolval-1){
+		for my $col(0..@{$mlcolval[$row]}-1) {
+			$transposed[$col][$row]=$mlcolval[$row][$col];
+		}
+	}
 	
+	# Sort the transposed array on the first row
+	@transposed = sort {
+		my $a1=$a->[$col];
+		my $b1=$b->[$col];
+		ncmp($a1 , $b1);
+	} @transposed;
+
+	# Transpose it back
+	for my $row (0..@transposed-1){
+		for my $col(0..@{$transposed[$row]}-1) {
+			$mlcolval[$col][$row]=$transposed[$row][$col];
+		}
+	}
+
+}
 
 
 1;
