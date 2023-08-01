7cc59613e2486d93d6f977e7310387bbad32ad17
ljmdullaart <ljm@acm.org>
2023-08-01 18:12:49 +0200

    debugging and vbox info.

--- itop_input.sh
+++ itop_input.sh
@@ -8,45 +8,45 @@ hvisors=itop/hvisors.$$
 
 mkdir -p itop
 i=1
-echo $i
+echo $i organization
 echo 'Name*,Code' > itop/$i-Organization.csv
 echo "$organization,none" >> itop/$i-Organization.csv
 
 i=$((i+1))
-echo $i
+echo $i OS_Family
 echo 'Name*' > itop/$i-OS_Family.csv
 sqlite3 $db  'SELECT DISTINCT ostype FROM server' | grep -v '^$' >> "itop/$i-OS_Family.csv"
 
 i=$((i+1))
-echo $i
+echo $i OS_Version
 echo "Name*,OS family->Name" > "itop/$i-OS_Version.csv"
 sqlite3 $db --separator ,  "SELECT DISTINCT os,ostype
                             FROM server" |
 	grep -v '^,*$' >> "itop/$i-OS_Version.csv"
 
 i=$((i+1))
-echo $i
+echo $i Server
 echo "Name*,OS family->Name,OS version->Name,CPU,RAM,Organization->Name" > "itop/$i-Server.csv"
 sqlite3 $db --separator ,  "SELECT name,ostype,os,processor,memory,'$organization'
                             FROM server
-                            WHERE ( options NOT LIKE '%vboxhost%' ) OR (options IS NULL) 
+                            WHERE (( options NOT LIKE '%vboxhost%' ) OR (options IS NULL) )
 " | grep -v '^,*$' | grep -v "^[0-9]" >> "itop/$i-Server.csv"
 
 i=$((i+1))
-echo $i
-echo "Name*,Organization->Name,Status"  > itop/$i-Hypervisors.csv
+echo $i Hypervisors
+echo "Name*,Organization->Name,Status,Server->Name"  > itop/$i-Hypervisors.csv
 sqlite3 $db "SELECT DISTINCT options
              FROM server
              WHERE options LIKE '%vboxhost:%'" |
 	sed 's/.*vboxhost://;s/,.*//'  > $tmp
 sort -u $tmp | while read host ; do
-	sqlite3 $db --separator ,  "SELECT name,'$organization','production'
+	sqlite3 $db --separator ,  "SELECT name,'$organization','production',name
                                     FROM server
                                     WHERE id=$host" >> itop/$i-Hypervisors.csv
 done
 
 i=$((i+1))
-echo $i
+echo $i Virtual_Machine
 sqlite3 $db --separator ','  "SELECT name,ostype,os,processor,memory,options
                               FROM server
                               WHERE  options LIKE '%vboxhost%' " > $tmp
@@ -62,5 +62,7 @@ sed 's/ /%/g;s/,/ /g'  $tmp | while read name ostype os processor memory option 
 
 done
 
+i=$((i+1))
+echo $i NAS
 
 rm -f $tmp $hvisors	
