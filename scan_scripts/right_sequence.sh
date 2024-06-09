0d65b4262f8dba992d22789d74053beac9fa6a94
ljmdullaart <ljm@acm.org>
2024-06-09 22:29:58 +0200

    added l2-scans

--- scan_scripts/right_sequence.sh
+++ scan_scripts/right_sequence.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+enter=no
+if [ "$1" = "-k" ] ; then
+	enter=yes
+fi
+
 if [ "a" = 'b' ] ; then 
 	:
 elif [ -f database/djedefre.db ] ;  then
@@ -17,10 +22,13 @@ cd /home/ljm/src/djedefre/scan_scripts
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-for script in local_system subnet arp access type server dhcp  remote_system cisco dns dhcp arp  vbox  local_system internet  clean_if 
+for script in local_system subnet arp access type server dhcp  remote_system cisco dns dhcp arp  vbox  local_system internet  clean_if l2top l2unifi l2vbox ifconnect
 do
-	bash $SCRIPTPATH/scan_$script.sh $dbfile
+	bash $SCRIPTPATH/scan_$script.sh $dbfile | sed "s/^/$script: /" 2>&1
 	echo "$script done"
+	if [ $enter = yes ] ; then
+		read line
+	fi
 done
 
 if [ -f webserver/database/djedefre.db ] ; then
