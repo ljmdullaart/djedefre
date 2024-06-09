7675c159b30239f59072217484fff370d809d8a6
ljmdullaart <ljm@acm.org>
2024-06-09 22:17:35 +0200

    complete rewrite, ready for removal.

--- l2input.pm
+++ l2input.pm
@@ -1,6 +1,6 @@
 
-#INSTALLEDFROM verlaine:/home/ljm/src/djedefre
 #INSTALL@ /opt/djedefre/l2input.pm
+#INSTALLEDFROM verlaine:/home/ljm/src/djedefre
 
 #  _     ____    _                   _   
 # | |   |___ \  (_)_ __  _ __  _   _| |_ 
@@ -14,267 +14,231 @@ require dje_db;
 our $main_window;
 our $mainframe;
 
-my @l2i_switchlist;
-my $l2i_selectedswitch='';
-my $l2i_addswitch='';
-my $l2i_addswitchid=-1;
-my $l2i_qtyports=16;
-my @l2i_connect;
-my @l2i_switchports;
-my @l2i_allswitchports;
-my @l2i_setif;
-my @l2i_setsw;
-my @l2i_disco;
-my @l2i_vlans;
-		
-my $l2i_buttonsettext='';
+our $DEB_FRAME;
+our $DEB_DB;
+our $DEB_SUB;
+our $DEBUG;
 
-sub l2i_add_a_switch {
-	(my $type)=@_;
-	if ($l2i_addswitch ne ''){
-		if(db_dosql("DELETE FROM switch WHERE name='$l2i_addswitch'")>0){print "l2i_add_a_switch sql error\n";}
-                if(db_dosql("INSERT INTO switch (name,ports) VALUES ('$l2i_addswitch',$l2i_qtyports)")>0){print "l2i_add_a_switch sql error\n";}
-	}
-	if (defined $type){
-		db_dosql("UPDATE switch SET switch='$type' WHERE name='$l2i_addswitch'");
-	}
-	l2input();
-}
+our @srv_devicetype;
+our @srv_interfaces;
+our @srv_last_up;
+our @srv_memory;
+our @srv_name;
+our @srv_options;
+our @srv_os;
+our @srv_ostype;
+our @srv_processor;
+our @srv_status;
+our @srv_type;
+our @srv_xcoord;
+our @srv_ycoord;
+our %srv_id;
 
-sub l2i_del_a_switch {
-	if ($l2i_addswitch ne ''){
-		if(db_dosql("DELETE FROM switch WHERE name='$l2i_addswitch'")>0){print "l2i_del_a_switch sql error\n";}
-		if(db_dosql("UPDATE interfaces SET switch=-1 WHERE switch=$l2i_addswitchid")>0){print "l2i_del_a_switch sql error\n";}
-		if(db_dosql("DELETE FROM l2connect WHERE from_tbl='switch' AND from_id=$l2i_addswitchid")>0){print "l2i_del_a_switch sql error\n";}
-		if(db_dosql("DELETE FROM l2connect WHERE to_tbl='switch' AND to_id=$l2i_addswitchid")>0){print "l2i_del_a_switch sql error\n";}
-	}
-	l2input();
-}
+our @devicetypes;
 
-sub l2i_disconnect {
-	(my $switch, my $port)=@_;
-	db_dosql ("SELECT from_tbl,from_id,from_port,to_tbl,to_id,to_port FROM l2connect WHERE from_tbl='switch' AND from_id=$switch AND from_port=$port");
-	if ((my $from_tbl,my $from_id,my $from_port,my $to_tbl,my $to_id,my $to_port)=db_getrow()){
-		if ($to_tbl eq 'interfaces'){
-			db_dosql ("UPDATE interfaces SET switch=-1 WHERE id=$to_id");
-			db_dosql ("DELETE FROM l2connect WHERE from_id=$switch AND from_port=$port");
-		}
-		elsif ($to_tbl eq 'switch'){
-			db_dosql ("DELETE FROM l2connect WHERE from_id=$switch AND from_port=$port");
-			db_dosql ("DELETE FROM l2connect WHERE to_id=$switch AND to_port=$port");
-		}
-	}
-	db_dosql ("SELECT from_tbl,from_id,from_port,to_tbl,to_id,to_port FROM l2connect WHERE to_tbl='switch' AND to_id=$switch AND to_port=$port");
-	if ((my $from_tbl,my $from_id,my $from_port,my $to_tbl,my $to_id,my $to_port)=db_getrow()){
-		db_dosql ("DELETE FROM l2connect WHERE from_id=$switch AND from_port=$port");
-		db_dosql ("DELETE FROM l2connect WHERE to_id=$switch AND to_port=$port");
-	}
-		
-}
+our @if_access;
+our @if_host;
+our @if_hostname;
+our @if_ifname;
+our @if_id;
+our @if_ip;
+our @if_macid;
+our @if_port;
+our @if_subnet;
+our @if_connect_if;
 
-sub l2i_reconnect{
+our @l2_id;
+our @l2_from_id;
+our @l2_from_port;
+our @l2_from_tbl;
+our @l2_to_id;
+our @l2_to_port;
+our @l2_to_tbl;
+our @l2_vlan;
+our @l2_source;
 
-	for my $i (0 .. $l2i_qtyports) {
-		if (defined $l2i_setif[$i]){
-			if ($l2i_setif[$i] ne ''){
-				(my $ifid,my $server,my $ip, my @mac)=split (':',$l2i_setif[$i]);
-				l2i_disconnect($l2i_addswitchid,$i);
-				$l2i_vlans[$i]=1 unless defined $l2i_vlans[$i];
-				if ($l2i_vlans[$i] eq ''){ $l2i_vlans[$i]=1; }
-				db_dosql "UPDATE interfaces SET switch=$l2i_addswitchid WHERE id=$ifid";
-				db_dosql "INSERT INTO l2connect (vlan,from_tbl,from_id,from_port,to_tbl,to_id,to_port) VALUES ($l2i_vlans[$i],'switch',$l2i_addswitchid,$i,'interfaces',$ifid,0)";
-			}
-		}
-	}
-	for my $i (0 .. $l2i_qtyports) {
-		if (defined $l2i_setsw[$i]){
-			if ($l2i_setsw[$i] ne ''){
-				(my $sw2,my $name,my $port)=split (':',$l2i_setsw[$i]);
-				$port=~s/port //;
-				l2i_disconnect($l2i_addswitchid,$i);
-				l2i_disconnect($sw2,$port);
-				$l2i_vlans[$i]=1 unless defined $l2i_vlans[$i];
-				if ($l2i_vlans[$i] eq ''){ $l2i_vlans[$i]=1; }
-				db_dosql "INSERT INTO l2connect (vlan,from_tbl,from_id,from_port,to_tbl,to_id,to_port) VALUES ($l2i_vlans[$i],'switch',$l2i_addswitchid,$i,'switch',$sw2,$port)";
-			}
-		}
-	}
-	for my $i (0 .. $l2i_qtyports) {
-		if (defined $l2i_vlans[$i]){
-			db_dosql ("UPDATE l2connect SET vlan='$l2i_vlans[$i]' WHERE from_id=$l2i_addswitchid AND from_port=$i");
-			db_dosql ("UPDATE l2connect SET vlan='$l2i_vlans[$i]' WHERE to_id=$l2i_addswitchid AND to_port=$i AND to_tbl='switch'");
-		}
-	}
-	for my $i (0 .. $l2i_qtyports) {
-		if ($l2i_disco[$i]>0){
-			l2i_disconnect($l2i_addswitchid,$i);
-		}
-	}
-	
 
-}
-	
+my $selected_connect;
+my $connectionlist_frame;
 
 sub l2input {
-	$l2i_addswitchid=-1;
-	if (defined $l2i_addswitch){
-		if ($l2i_addswitch ne ''){
-			db_dosql("SELECT id FROM switch WHERE name='$l2i_addswitch'");
-			($l2i_addswitchid)=db_getrow();
-		}
-	}
-	splice @l2i_setif;
-	splice @l2i_setsw;
+	debug($DEB_SUB,"l2input");
 	$Message='';
 	$main_frame->destroy if Tk::Exists($main_frame);
 	$main_frame=$main_window->Frame(
 		#-height      => 0.8*$main_window_height,
 		#-width       => $main_window_width
 	)->pack(-side =>'top');
-	my $switchframe=$main_frame->Frame()->pack(-side =>'left');
-	my $portframe = $main_frame->Scrolled("Frame",-scrollbars=>'e',-height=>400)->pack(-side =>'right');
-	my $sth=db_dosql("SELECT name FROM switch");
-	my $i=0;
-	splice @l2i_switchlist;
-	while((my $name) = db_getrow()){
-		$l2i_switchlist[$i]=$name;
-		$i++;
-	}
-	$switchframe->Label(-text=> 'Switch')->pack(-side=>'top');
-	my $switchlistbox=$switchframe->Scrolled("Listbox", -scrollbars=>'e',-width=>28,-height=>20)->pack(-side=>'top');
-	$switchlistbox->insert('end',@l2i_switchlist);
-	$switchlistbox->bind('<<ListboxSelect>>' => sub {my @sel=$switchlistbox->curselection;$l2i_addswitch=$l2i_switchlist[$sel[0]];l2input();});
+	$connectionlist_frame->destroy if Tk::Exists($connectionlist_frame);
+	$connectionlist_frame=$main_frame->Frame()->pack(-side =>'top');
+	mkl2connectframe($connectionlist_frame);
+	mkl2selectedframe($connectionlist_frame);
+}
 	
-	my @portoptions=(5,6,8,13,15,16,24,48,64);
-	$switchframe->Optionmenu(-variable=>\$l2i_qtyports, -options=>\@portoptions,-width=>27)->pack(-side=>'top');
-
-	$switchframe->Entry ( -width=>32,-textvariable=>\$l2i_addswitch)->pack(-side=>'top');
-
-	my $buttoswitchframe=$switchframe->Frame()->pack(-side=>'top');
-	$buttoswitchframe->Button ( -width=>15,-text=>'Add switch',      -command=>sub{ l2i_add_a_switch('switch');})->pack(-side=>'left');
-	$buttoswitchframe->Button ( -width=>15,-text=>'Add Accesspoint', -command=>sub{ l2i_add_a_switch('accesspoint');})->pack(-side=>'left');
-	my $buttoswitchframe=$switchframe->Frame()->pack(-side=>'top');
-	$buttoswitchframe->Button ( -width=>15,-text=>'Delete',          -command=>sub{ l2i_del_a_switch();})->pack(-side=>'left');
-	$buttoswitchframe->Button ( -width=>15,-text=>'Set qty ports',   -command=>sub{ l2i_add_a_switch();})->pack(-side=>'left');
-
-	my $sth=db_dosql("SELECT id,ports FROM switch WHERE name='$l2i_addswitch'");
-	(my $switchid,$l2i_qtyports)=db_getrow();
-	$switchid=0 unless defined $switchid;
-	$l2i_qtyports=16 unless defined $l2i_qtyports;
-	for my $i (0 .. $l2i_qtyports) { $l2i_connect[$i]='Not connected';}
-	my @hostnames;
-	splice @hostnames;
-	$sth=db_dosql("SELECT id,name FROM server");
-	while ((my $id,my $name)= db_getrow()){
-		$hostnames[$id]=$name;
-	}
-	my @freeif;
-	splice @freeif;
-	push @freeif,'';
-	$sth=db_dosql("SELECT id,macid,ip,host FROM interfaces");
-	while ((my $id,my $macid,my $ip,my $host)= db_getrow()){
-		my $server=' ';
-		if (defined $hostnames[$host]){$server=$hostnames[$host];}
-		push @freeif,"$id:$server:$ip:$macid";
-	}
-	my @tmp=sort { (split(/:/,$a))[1] cmp (split(/:/,$b))[1]} @freeif;
-	@freeif=@tmp;
-	splice @l2i_allswitchports;
-	push @l2i_allswitchports,'';
-	db_dosql("SELECT id,name,ports FROM switch");
-	while ((my $id, my $switch, my $ports)=db_getrow()){
-		for my $i (0 .. $ports) {
-			push @l2i_allswitchports,"$id:$switch:port $i";
-		}
-	}
-	#
-	# "from" is always a switchport. "to" is either an interface or a switchport
-	#
-	splice @l2i_vlans;
-	$sth=db_dosql("SELECT vlan,from_port,to_tbl,to_id,to_port FROM l2connect WHERE from_tbl='switch' AND from_id=$switchid");
-	while ((my $vlan,my $from_port,my $to_tbl,my $to_id,my $to_port)= db_getrow()){
-		if ($to_tbl eq 'switch'){
-			$l2i_connect[$from_port]="switch:$to_id:$to_port";
-		}
-		elsif($to_tbl eq 'interfaces'){
-			$l2i_connect[$from_port]="interfaces:$to_id:0";
-		}
-		$l2i_vlans[$from_port]=$vlan;
-	}
-	db_dosql("SELECT vlan,from_port,from_id,from_tbl,to_id,to_port FROM l2connect WHERE to_tbl='switch' AND to_id=$switchid");
-	while ((my $vlan,my $from_port,my $from_id,my $from_tbl,my $to_id,my $to_port)= db_getrow()){
-		if ($from_tbl eq 'switch'){
-			$l2i_connect[$to_port]="switch:$from_id:$from_port";
-		}
-		$vlan=0 unless defined $vlan;
-		$l2i_vlans[$from_port]=$vlan;
-	}
-	for my $i (0 .. $#l2i_connect){
-		(my $type, my $id, my $port)=split (':',$l2i_connect[$i]);
-		if (!($id=~/^[0-9]+$/)){} # l2i_connect[$i] can be "Not connected"
-		elsif ($type eq 'interfaces'){
-			db_dosql ("SELECT host,ip FROM interfaces WHERE id=$id");
-			if ((my $host,my $ip)=db_getrow()){
-				db_dosql ("SELECT name FROM server WHERE id=$host");
-				(my $name)=db_getrow();
-				$name=$ip unless defined $name;
-				$l2i_connect[$i]="$name:$type:$ip:$port";
-			}
-		}
-		elsif ($type eq 'switch'){
-			db_dosql ("SELECT name FROM switch WHERE id=$id");
-			if ((my $name)=db_getrow()){
-				$l2i_connect[$i]="switch:$name port:$port";
-			}
-		}
-	}
-	my $portbuttonframe=$portframe->Frame()->pack(-side=>'top');
-	$portbuttonframe->Label ( -text=>'Switch:')->pack(-side=>'left');
-	$portbuttonframe->Label ( -textvariable=>\$l2i_addswitch,-justify => 'left', -width=>50)->pack(-side=>'left');
-	$portbuttonframe->Button (
-		-text=>'(re/dis)connect all changes',
-		-command=>sub {l2i_reconnect();}
-	)->pack(-side=>'right');
-	my $porttitleframe=$portframe->Frame()->pack(-side=>'top');
-	$porttitleframe->Label (-text=>"Port     ",-width=>10)->pack(-side=>'left');
-	$porttitleframe->Label (-text=>'Current connection',-width=>50)->pack(-side=>'left');
-	$porttitleframe->Label (-text=>'Interfaces',-width=>25)->pack(-side=>'left');
-	$porttitleframe->Label (-text=>'Switches',-width=>25)->pack(-side=>'left');
-	$porttitleframe->Label (-text=>'VLAN',-width=>25)->pack(-side=>'left');
-	$porttitleframe->Label (-text=>'  ',-width=>12)->pack(-side=>'left');
-	for my $i (0 .. $l2i_qtyports) {
-		my $perportframe=$portframe->Frame(
-			-borderwidth	=>2,
-			-relief		=>'groove'
-		)->pack(-side=>'top');
-		$perportframe->Label (-text=>"Port $i",-width=>10)->pack(-side=>'left');
-		$perportframe->Label (-text=>$l2i_connect[$i],-width=>50)->pack(-side=>'left');		# current connection
-		my $selifdrop=$perportframe->JBrowseEntry(						# New interface to connect
-			-variable => \$l2i_setif[$i],
-			-width=>25,
-			-choices => \@freeif,
-			-height=>10,
-			-browsecmd => sub {
-				$l2i_buttonsettext="Connect port $i on switch $l2i_addswitch to ${\$l2i_setif[$i]}";
-			}
-		)->pack(-side=>'left');
-		my $selswdrop=$perportframe->JBrowseEntry(						# New interface to connect
-			-variable => \$l2i_setsw[$i],
-			-width=>25,
-			-choices => \@l2i_allswitchports,
-			-height=>10,
-			-browsecmd => sub {
-				print "connect to switchport $l2i_setsw[$i]";
-			}
-		)->pack(-side=>'left');
-		$perportframe->Entry(-textvariable=>\$l2i_vlans[$i], -width=>25)->pack(-side=>'left');
-		$l2i_disco[$i]=0;
-		$perportframe->Checkbutton(-text => 'Disconnect', -variable => \$l2i_disco[$i], -width=>10)->pack(-side=>'left');
-		
-	}
-
+my $l2_selected_frame;
+my $l2_selected_entry_frame;
+my $l2_selected_button_frame;
+sub mkl2selectedframe {
+	(my $parent)=@_;
+	$l2_selected_frame->destroy if Tk::Exists($l2_selected_frame);
+	$l2_selected_frame=$parent->Frame()->pack(-side =>'bottom');
+	$l2_selected_entry_frame=$l2_selected_frame->Frame()->pack(-side =>'top');
+	$l2_selected_entry_frame->Entry ( -textvariable => \$id,        -width => 5,-font=>"arial 14")->pack(-side=>left);
+	$l2_selected_entry_frame->Entry ( -textvariable => \$from_tbl,  -width =>10,-font=>"arial 14")->pack(-side=>left);
+	$l2_selected_entry_frame->Entry ( -textvariable => \$fromhost,  -width =>15,-font=>"arial 14")->pack(-side=>left);
+	$l2_selected_entry_frame->Entry ( -textvariable => \$fromif,    -width =>15,-font=>"arial 14")->pack(-side=>left);
+	$l2_selected_entry_frame->Entry ( -textvariable => \$from_port, -width => 5,-font=>"arial 14")->pack(-side=>left);
+	$l2_selected_entry_frame->Entry ( -textvariable => \$to_tbl,    -width =>10,-font=>"arial 14")->pack(-side=>left);
+	$l2_selected_entry_frame->Entry ( -textvariable => \$tohost,    -width =>15,-font=>"arial 14")->pack(-side=>left);
+	$l2_selected_entry_frame->Entry ( -textvariable => \$toif,      -width =>15,-font=>"arial 14")->pack(-side=>left);
+	$l2_selected_entry_frame->Entry ( -textvariable => \$to_port,   -width => 5,-font=>"arial 14")->pack(-side=>left);
+	$l2_selected_entry_frame->Entry ( -textvariable => \$to_port,   -width =>10,-font=>"arial 14")->pack(-side=>left);
+	$l2_selected_entry_frame->Label(-text=>'.',-font=>"arial 14",-width=>2)->pack(-side=>left);
+	$l2_selected_button_frame=$l2_selected_frame->Frame()->pack(-side =>'bottom');
+	$l2_selected_button_frame->Button ( -width=>20,-text=>'Add'   )->pack(-side=>left);
+	$l2_selected_button_frame->Button ( -width=>20,-text=>'Change')->pack(-side=>left);
+	$l2_selected_button_frame->Button ( -width=>20,-text=>'Delete', -command=>sub{ print "del\n";l2deleteentry(); mkl2connectframe($connectionlist_frame); })->pack(-side=>left);
+}
+sub l2deleteentry {
+	(my $id)=@_;
+	print "DELETE FROM l2connect WHERE id=$id\n";
+	db_dosql ("DELETE FROM l2connect WHERE id=$id");
+	undef $ids[$id];
 }
 
-		
+sub l2changeentry{
+	if ($from_tbl =~/^i/){$from_tbl='interfaces';}
+	elsif ($from_tbl =~/^a/){$from_tbl='switch';}
+	elsif ($from_tbl =~/^s/){$from_tbl='switch';}
+	# to be done
+	db_get_interfaces();
+	db_get_server();
+	db_get_l2();
+	
+}
 
+my $l2_listing_frame;
+sub mkl2connectframe {
+	(my $parent)=@_;
+	db_get_interfaces();
+	db_get_server();
+	db_get_l2();
+	db_get_sw();
+	splice @fromhosts;
+	splice @tohosts;
+	
+	$l2_listing_frame->destroy if Tk::Exists($l2_listing_frame);
+	$l2_listing_frame=$parent->Frame()->pack(-side =>'top');
+	$l2_listing_frame->Label(-text=>"Connections")->pack()-side =>'top';
+	ml_new($l2_listing_frame,25,'top');
+	my @ar;
+	$ar[0]= 5;	#id
+	$ar[1]=10;	#from type
+	$ar[2]=10;	#from type
+	$ar[3]=15;	#from host
+	$ar[4]=15;	#from interface
+	$ar[5]= 5;	#from port
+	$ar[6]=10;	#to type
+	$ar[7]=15;	#to host
+	$ar[8]=15;	#to Interface
+	$ar[9]= 5;	#to port
+	$ar[10]=10;	#vlan
+	ml_colwidth(@ar);
+	splice @ar;
+	@ar=('ID','From',"IF",'Name','Interface','Port','To','Name','Interface','Port','VLAN');
+	ml_colhead(@ar);
+	foreach my $id (@l2_id){
+		next unless defined $id;
+		my $fromhost='';
+		my $fromif='';
+		my $fromifname='';
+		my $from_id=$l2_from_id[$id];
+		if ( $l2_from_tbl[$id]  eq 'switch' ){
+			$fromhost=$sw_name[$from_id];
+		}
+		else {
+			my $fromhostid=$if_host[$from_id];
+			if (defined ($srv_name[$fromhostid])) {
+				$fromhosts[$id]=$srv_name[$fromhostid];
+			}
+			else {
+				$fromhosts[$id]=$fromid;
+			}
+			$fromif=$if_ip[$from_id];
+			$fromifname=$if_ifname[$from_id] if defined $if_ifname[$from_id];
+		}
+		my $tohost='';
+		my $toif='';
+		my $to_id=$l2_to_id[$id];
+		if ( $l2_to_tbl[$id] eq 'switch' ){
+			$tohost=$sw_name[$to_id];
+			$to_if='';
+		}
+		else {
+			my $tohostid=$if_host[$to_id];
+			$tohosts[$id]=$srv_name[$tohostid];
+			$toif=$if_ip[$to_id];
+		}
+		
+		$from_ifnames[$id]=$fromifname;
+		$tohosts[$id]=$tohost;
+	}
+	foreach my $id (@l2_id){
+		next unless defined $id;
+		splice @ar;
+		$ar[0]='';
+		$ar[1]='';
+		$ar[2]='';
+		$ar[3]='';
+		$ar[4]='';
+		$ar[5]='';
+		$ar[6]='';
+		$ar[7]='';
+		$ar[8]='';
+		$ar[9]='';
+		$ar[10]='';
+		$ar[0]=$id               if defined $id;
+		$ar[1]=$l2_from_tbl[$id] if defined $l2_from_tbl[$id];
+		$ar[2]=$from_ifnames[$id]if defined $from_ifnames[$id];
+		$ar[3]=$fromhosts[$id]   if defined $fromhosts[$id];
+		$ar[4]=$l2_from_id[$id]  if defined $l2_from_id[$id];
+		$ar[5]=$l2_from_port[$i] if defined $l2_from_port[$i];
+		$ar[6]=$l2_to_tbl[$id]   if defined $l2_to_tbl[$id];
+		$ar[7]=$tohosts[$id]     if defined $tohosts[$id];
+		$ar[8]=$l2_to_id[$id]    if defined $l2_to_id[$id];
+		$ar[9]=$l2_to_port[$id]  if defined $l2_to_port[$id];
+		$ar[10]=$l2_vlan[$id]     if defined $l2_vlan[$id];
+		ml_insert(@ar);
+
+	}
+	ml_create();
+	ml_callback(\&l2connect_callback);
+}
+
+
+sub l2connect_callback {
+	(my $i)=@_;
+	$id        =   '';
+	$from_tbl  =   '';
+	$fromhosts =   '';
+	$fromif    =   '';
+	$fromif    =   '';
+	$from_port =   '';
+	$to_tbl    =   '';
+	$tohost    =   '';
+	$toif      =   '';
+	$to_port   =   '';
+	$vlan      =   '';
+	$id       = $ids[$i]        if defined $ids[$i];
+	$from_tbl = $from_tbls[$i]  if defined $from_tbls[$i];
+	$fromhost = $fromhosts[$i]  if defined $fromhosts[$i];
+	$fromif   = $fromifs[$i]    if defined $fromifs[$i];
+	$from_port= $from_ports[$i] if defined $from_ports[$i];
+	$to_tbl   = $to_tbls[$i]    if defined $to_tbls[$i];
+	$tohost   = $tohosts[$i]    if defined $tohosts[$i];
+	$toif     = $toifs[$i]      if defined $toifs[$i];
+	$to_port  = $to_ports[$i]   if defined $to_ports[$i];
+	$vlan     = $vlans[$i]      if defined $vlans[$i];
+}
+	
 1;
