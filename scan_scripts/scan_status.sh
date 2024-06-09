e62bf0343468a5a48bd209ef4ccd641c1112e616
ljmdullaart <ljm@acm.org>
2024-06-09 22:25:12 +0200

    use variable LOG in stead of fixed logfile

--- scan_scripts/scan_status.sh
+++ scan_scripts/scan_status.sh
@@ -3,6 +3,8 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 tmp=/tmp/scan_status.$$
 NOW=$(date -Iseconds | sed 's/+.*//')
+LOG=/tmp/status.check.log
+date > $LOG
 
 if [ -f $SCRIPTPATH/djedefre.common.sh ] ; then
 	. $SCRIPTPATH/djedefre.common.sh
@@ -19,13 +21,21 @@ elif [ "$1" != '' ] ; then
 	fi
 fi
 
-date > status.check.log
 
 previnet=$(sqlite3 "$database" "SELECT value FROM config WHERE attribute='run:param' AND item='inetup'")
 if [ "$previnet" = "" ] ; then
-	sqlite3 "$database" "INSERT INTO config (attribute,item,value) VALUES ('run:param','inetup','unknown')"
+	sleep 1
+	previnet=$(sqlite3 "$database" "SELECT value FROM config WHERE attribute='run:param' AND item='inetup'")
+	if [ "$previnet" = "" ] ; then
+		sleep 1
+		previnet=$(sqlite3 "$database" "SELECT value FROM config WHERE attribute='run:param' AND item='inetup'")
+		if [ "$previnet" = "" ] ; then
+			sqlite3 "$database" "INSERT INTO config (attribute,item,value) VALUES ('run:param','inetup','unknown')"
+		fi
+	fi
 fi
 if ping -c1 8.8.8.8 > /dev/null 2>&1 ; then
+	echo "Internet is up."
 	if [ "$previnet" = "up" ] ; then
 		:
 	else 
@@ -33,6 +43,7 @@ if ping -c1 8.8.8.8 > /dev/null 2>&1 ; then
 		sqlite3 "$database" "UPDATE config SET value='yes' WHERE attribute='run:param' AND item='changed'"
 	fi
 else
+	echo "Internet is down."
 	if [ "$previnet" = "up" ] ; then
 		sqlite3 "$database" "UPDATE config SET value='down' WHERE attribute='run:param' AND item='inetup'"
 		sqlite3 "$database" "UPDATE config SET value='yes' WHERE attribute='run:param' AND item='changed'"
@@ -45,50 +56,56 @@ for server_id in $(cat $tmp) ; do
 	name=$(sqlite3 "$database" "SELECT name FROM server WHERE id=$server_id")
 	stat=$(sqlite3 "$database" "SELECT status FROM server WHERE id=$server_id")
 	
-echo "$name($server_id)=$stat"
+echo "$name($server_id)=$stat" >> $LOG
 	if [ "$stat" = "excluded" ] ; then
 		:
 echo "	excluded"
 	elif [ -f "$SCRIPTPATH/status_$name.sh" ] ; then
-		echo "Script for $name" >> status.check.log
+		echo "Script for $name" >> $LOG
 		if bash "$SCRIPTPATH/status_$name.sh" ; then
 			sqlite3 "$database" "UPDATE server SET status='up' WHERE id=$server_id"
 			sqlite3 "$database" "UPDATE server SET last_up='$NOW' WHERE id=$server_id"
-			echo "    $name up" >> status.check.log
+			echo "    $name up" >> $LOG
 		else
 			sqlite3 "$database" "UPDATE server SET status='down' WHERE id=$server_id"
-			echo "    $name down" >> status.check.log
+			echo "    $name down" >> $LOG
 		fi
 	else
-		echo "Ping for $name" >> status.check.log
+		echo "Ping for $name" >> $LOG
 		up=0
 		for interface in $(sqlite3 "$database" "SELECT ip FROM interfaces WHERE host=$server_id") ; do
-			if ping -c1 -W1 -q $interface  >/dev/null 2> /dev/null ; then
+			echo "    $interface"
+			echo "    "
+			if echo -n '1' && ping -c1 -W1 -q $interface  >/dev/null 2> /dev/null ; then
+				echo " $interface ping is ok"
 				up=1
-			elif ping -c1 -W1 -q $interface  >/dev/null 2> /dev/null ; then
+			elif echo -n '2' && ping -c1 -W1 -q $interface  >/dev/null 2> /dev/null ; then
+				echo " $interface ping is ok"
 				up=1
-			elif arping -c1 -q $interface  >/dev/null 2> /dev/null ; then
+			elif echo -n '3' && arping -c1 -w1 -q $interface  >/dev/null 2> /dev/null ; then
+				echo " $interface arping is ok"
 				up=1
-			elif arping -c1 -q $interface  >/dev/null 2> /dev/null ; then
+			elif echo -n '4' && arping -c1 -w1 -q $interface  >/dev/null 2> /dev/null ; then
+				echo " $interface arping is ok"
 				up=1
-			elif wget -qO-  $interface  >/dev/null 2> /dev/null ; then
+			elif echo -n '6' && ping -c1 -W1 -q $interface  >/dev/null 2> /dev/null ; then
+				echo " $interface ping is ok"
 				up=1
-			elif ping -c1 -W1 -q $interface  >/dev/null 2> /dev/null ; then
-				up=1
-			elif nmap -Pn $interface -p 2968 | grep -q open ; then
+			elif echo -n '7' && nmap -Pn $interface -p 2968 | grep -q open ; then
+				echo " $interface nmap is ok"
 				up=1
 			fi
 		done
 		if [ $up = 1 ] ; then
 			sqlite3 "$database" "UPDATE server SET status='up' WHERE id=$server_id"
 			sqlite3 "$database" "UPDATE server SET last_up='$NOW' WHERE id=$server_id"
-			echo "    $name up" >> status.check.log
+			echo "    $name up" >> $LOG
 			if [ "$stat" != "up" ] ; then
 				sqlite3 "$database" "UPDATE config SET value='yes' WHERE  attribute='run:param' AND item='changed'"
 			fi
 		else
 			sqlite3 "$database" "UPDATE server SET status='down' WHERE id=$server_id"
-			echo "    $name down" >> status.check.log
+			echo "    $name down" >> $LOG
 			if [ "$stat" = "up" ] ; then
 				sqlite3 "$database" "UPDATE config SET value='yes' WHERE  attribute='run:param' AND item='changed'"
 			fi
