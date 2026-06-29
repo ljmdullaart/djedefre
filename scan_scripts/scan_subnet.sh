15c8fdaf5d2f0f61a27e08a7910788b2585d74ef
ljmdullaart <ljm@acm.org>
2026-06-29 17:02:54 +0200

    rewrite

--- scan_scripts/scan_subnet.sh
+++ scan_scripts/scan_subnet.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-tmp=/tmp/scan_subnet.$$
-tmp1=/tmp/scan_subnet1.$$
+tmpip=/tmp/scan_subnet.$$
+tmpnets=/tmp/scan_subnet1.$$
+tmpint=/tmp/scan_subnet2.$$
+
+NOW=$(date -Iseconds | sed 's/+.*//')
 
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
@@ -21,42 +24,72 @@ elif [ "$1" != '' ] ; then
 	fi
 fi
 
-if [ "$2" = "" ] ; then
-	sqlite3  -separator ' ' -cmd ".timeout 1000"  "$database" "SELECT id,nwaddress,cidr FROM subnet" > $tmp1
-else
-	sqlite3  -separator ' ' -cmd ".timeout 1000"  "$database" "SELECT id,nwaddress,cidr FROM subnet WHERE id=$2" > $tmp1
-fi
-echo Scan subnets
-sed 's/^/    /' $tmp1
-echo '-------------------'
-grep -v Internet $tmp1 | while read id ip cidr ; do
+list_subnet > $tmpnets
 
-		echo  "    $ip / $cidr"
-		if [ "$cidr" != '' ] ; then
-			if [ $cidr -gt 22 ] ; then
-				sudo fping -g $ip/$cidr 2>&1 | sed -n 's/ is alive//p'>> $tmp
+
+djedefre_log "Scan subnets"
+sed 's/^/    /' $tmpnets
+djedefre_log '-------------------'
+egrep -v 'Internet|id' $tmpnets | cut -d' ' -f1-3  | while read id ip cidr ; do
+		ip_scope=$(valfromid subnet $id scope)
+		if [ "$ip_scope" = "local" ] || [ "$ip_scope" = "global" ] ; then
+			echo -n .
+			djedefre_log  "    $ip / $cidr"
+			if [ "$cidr" != '' ] ; then
+				if [ $cidr -gt 20 ] ; then
+					sudo fping -g $ip/$cidr 2>&1 | sed -n "s/ is alive/ $id $ip_scope/p">> $tmpip
+				fi
 			fi
 		fi
-	done
-	echo
+	done 
+echo
+djedefre_log "Add interfaces"
+touch $tmpip
 
-
-sort -u $tmp | while read if id; do
-	ifid=$(sqlite3  -separator ' ' -cmd ".timeout 1000"  "$database" "SELECT id FROM interfaces WHERE ip='$if'")
-	if [ "$ifid" = "" ] ; then
-		sqlite3  -separator ' '  $database "INSERT INTO interfaces (ip) VALUES ('$if')"
-	fi
-	sqlite3 -cmd ".timeout 1000"  "$database" "UPDATE interfaces SET subnet='$id' WHERE ip='$if'"
-	host=$(sqlite3  -separator ' ' -cmd ".timeout 1000"  "$database" "SELECT host FROM interfaces WHERE ip='$if'")
-	if [ "$host" = "" ] ; then
-		add_server $if
-		sqlite3 -cmd ".timeout 1000"  "$database" "UPDATE interfaces SET host=$db_retval  WHERE ip='$if'"
-		sqlite3  $database "UPDATE config SET value='yes' WHERE attribute='run:param' AND item='changed'"
-	fi
+sort -u $tmpip | while read ip subnetid ip_scope; do
+	declare -A interface_data
+	interface_data[ip]="$ip"
+	interface_data[ip_scope]="$ip_scope"
+	interface_data[subnet]="$subnetid"
+	interface_data[source]="scan_subnet"
+	set_interface interface_data
+	unset interface_data
+	echo -n .
 	
 done
+echo
 
-if_net;
+idfrom_interfaces host NULL > $tmpint
 
-rm -f $tmp
-rm -f $tmp1
+cat $tmpint | while read interfaceid ; do
+	ip=$(valfromid interfaces $interfaceid ip)
+	name=$(host $ip | grep -v NXDOMAIN |sed 's/.* //;s/\..*//')
+	if [ "$name" = "" ] ; then
+		name="$ip"
+	fi
+	echo "$interfaceid: $ip $name"
+	declare -A server_data
+
+	server_data[name]="$name"
+	server_data[type]="server"
+	server_data[last_up]="$NOW"
+	server_data[source]="scan_subnet"
+	set_server server_data
+	serverid=$db_retval
+	unset server_data
+
+	declare -A interface_data
+	interface_data[ip]="$ip"
+	interface_data[id]="$interfaceid"
+	interface_data[host]="$serverid"
+	interface_data[source]="scan_subnet"
+	set_interface interface_data
+	unset interface_data
+	
+
+	echo "$interfaceid: $ip $name server:$serverid in $db_retval"
+done
+
+rm -f $tmpip
+rm -f $tmpnets
+rm -f $tmpint
