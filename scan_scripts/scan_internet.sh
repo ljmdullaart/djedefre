a9a068f7957314fe8a2f857a87c143dc8a996609
ljmdullaart <ljm@acm.org>
2026-06-29 16:23:47 +0200

    new datavase access routines

--- scan_scripts/scan_internet.sh
+++ scan_scripts/scan_internet.sh
@@ -18,15 +18,14 @@ elif [ "$1" != '' ] ; then
 	fi
 fi
 
-if sqlite3 $database  'SELECT nwaddress FROM subnet' |grep Internet ; then
-        echo "Internet is present"
-elif ping -c1 8.8.8.8 > /dev/null 2>/dev/null ; then
-        sqlite3 $database  "INSERT INTO subnet (nwaddress,name) VALUES ('Internet','Internet')"
-	sqlite3  $database "UPDATE config SET value='yes' WHERE attribute='run:param' AND item='changed'"
-else
-        echo "No Internet."
-	exit 0
-fi
+
+declare -A subnet_data
+subnet_data[name]="Internet"
+subnet_data[nwaddress]="Internet"
+set_subnet subnet_data
+internetsn_id=$dbretval
+unset subnet_data
+
 
 tmpfile=/tmp/scan_internet.$$
 tmpfile2=/tmp/scan_internet2.$$
@@ -36,25 +35,26 @@ traceroute 8.8.8.8                |
 	sed -n 's/.*(//;s/).*//p' >>$tmpfile2
 
 lastif=$( ip route get 8.8.8.8 | sed -n 's/.*src //;s/ .*//;1p')
-lastid=$(sqlite3 $database "SELECT id FROM interfaces WHERE ip='$lastif'" )
-if [ "$lastid" = "" ] ; then
+lastif_id=$(idfromval interfaces ip "$lastif" )
+if [ "$lastif_id" = "" ] ; then
 	echo "Cannot determine the last host before Internet"
 	exit 
 fi
 
 path="$lastif"
-idpath=$(sqlite3 $database "SELECT host FROM interfaces WHERE ip='$lastif'" )
+pathif_id=$(idfromval interfaces ip "$lastif" )
+pathif_host=$(valfromid interfaces $pathif_id host)
 
 for ip in $(cat $tmpfile2) ; do
-	next=$(sqlite3 $database "SELECT id FROM interfaces WHERE ip='$ip'" )
+	nextif_id=$(idfromval interfaces ip "$ip")
 	echo -n "SCAN INTERNET  $ip-> $next ; "
-	if [ "$next" != "" ] ; then
-		nexthost=$(sqlite3 $database "SELECT host FROM interfaces WHERE ip='$ip'" )
-		nextname=$(sqlite3 $database "SELECT name FROM server WHERE id=$nexthost")
+	if [ "$nextif_id" != "" ] ; then
+		nextif_host=$(valfromid interfaces $nextif_id host)
+		nextsrv_name=$(valfromid server $nextif_host name)
 		lastif=$ip
-		lastid=$next
-		path="$path:$nextname"
-		idpath="$idpath:$nexthost"
+		lastif_id=$nextif_id
+		path="$path:$nextsrv_name"
+		idpath="$idpath:$nextif_host"
 	fi
 	echo "lastif=$lastif"
 done
@@ -62,27 +62,24 @@ done
 rm -f $tmpfile
 rm -f $tmpfile2
 
-lasthost=$(sqlite3 $database "SELECT host FROM interfaces WHERE id='$lastid'")
+echo $path
 
-echo  -n 'The last host is '
-sqlite3 $database "SELECT * FROM server WHERE id=$lasthost"
 echo "Path=$path"
 echo "Idpath=$idpath"
-if sqlite3 $database  "SELECT item FROM config WHERE attribute='run:param' AND item='idpath'" |grep idpath ; then
-        sqlite3  $database "UPDATE config SET value='$idpath' WHERE attribute='run:param' AND item='idpath'"
-else
-        sqlite3 $database  "INSERT INTO config (attribute,item,value) VALUES ('run:param','idpath','$idpath')"
-fi
+set_dbconfig 'run:param' 'idpath' "$idpath"
 
-inetnet=$(sqlite3 $database "SELECT id FROM subnet WHERE nwaddress='Internet'")
-inetif=$(sqlite3 $database "SELECT id FROM interfaces WHERE ip='Internet'")
-inethost=$(sqlite3 $database "SELECT host FROM interfaces WHERE ip='Internet'")
+internetsn_id=$(idfromval subnet nwaddress Internet)
+echo  "subnet : $internetsn_id"
+echo  "host nr: $nextif_host"
+echo  "host   : $nextsrv_name"
+echo  "ip     : Internet"
+echo  "path   : $path"
 
-if [ "$inetif" = "" ] ; then
-	sqlite3 $database  "INSERT INTO interfaces (host,subnet,ip,switch) VALUES ($lasthost,$inetnet,'Internet',-1)"
-	sqlite3  $database "UPDATE config SET value='yes' WHERE attribute='run:param' AND item='changed'"
-else
-	sqlite3 $database "UPDATE interfaces SET host=$lasthost WHERE ip='Internet'"
-	sqlite3  $database "UPDATE config SET value='yes' WHERE attribute='run:param' AND item='changed'"
-fi
 
+
+declare -A interface_data
+interface_data[ip]="Internet"
+interface_data[host]="$nextif_host"
+interface_data[subnet]="$internetsn_id"
+set_interface interface_data
+unset interface_data
