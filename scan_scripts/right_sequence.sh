f30eed6829a5171ad070da3e4e29a7d4c14f500e
ljmdullaart <ljm@acm.org>
2026-06-29 16:20:32 +0200

    Use improved scan scripting.

--- scan_scripts/right_sequence.sh
+++ scan_scripts/right_sequence.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+logfile='djedefre.log'
 enter=no
 if [ "$1" = "-k" ] ; then
 	enter=yes
 fi
+
+echo '--- START ---' > $logfile
 
 if [ "a" = 'b' ] ; then 
 	:
@@ -22,15 +25,16 @@ cd /home/ljm/src/djedefre/scan_scripts
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-for script in local_system subnet arp access type server dhcp  remote_system cisco dns dhcp arp  vbox  local_system internet  clean_if l2top l2unifi l2vbox ifconnect
+#for script in local_system subnet access arp type server  remote_system cisco dns dhcp arp  vbox  local_system internet  clean_if l2top l2unifi l2vbox ifconnect dhcp
+for  script in local_system subnet access arp type mergeif server serverinfo vbox internet arp l2scans data
 do
-	echo "-----STARTING $script"
+	echo "-----STARTING $script" >>$logfile
 	date
 	timeout 300 bash $SCRIPTPATH/scan_$script.sh $dbfile 2>&1 | sed "s/^/$script: /" 2>&1
 	if [ $? = 124 ] ; then
-		echo "-----$script killed"
+		echo "-----$script killed" >>$logfile
 	else
-		echo "-----$script DONE"
+		echo "-----$script DONE" >>$logfile
 	fi
 	if [ $enter = yes ] ; then
 		read line
@@ -38,13 +42,13 @@ do
 done
 
 for script in page_*.sh ; do
-	echo "-----STARTING $script"
+	echo "-----STARTING $script" >>$logfile
 	date
 	timeout 300 bash $SCRIPTPATH/$script $dbfile | sed "s/^/$script: /" 2>&1
 	if [ $? = 124 ] ; then
-		echo "-----$script killed"
+		echo "-----$script killed" >>$logfile
 	else
-		echo "-----$script DONE"
+		echo "-----$script DONE" >>$logfile
 	fi
 	if [ $enter = yes ] ; then
 		read line
