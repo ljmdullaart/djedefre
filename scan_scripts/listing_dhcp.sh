828716379d79a9eee05a43fa6dbc55f3e4520338
ljmdullaart <ljm@acm.org>
2026-07-31 14:01:49 +0200

    added htm as output.

--- scan_scripts/listing_dhcp.sh
+++ scan_scripts/listing_dhcp.sh
@@ -1,24 +1,50 @@
 #!/bin/bash
 
-echo "               DHCP HOSTS"
-echo "-----------------------------------------"
-ssh root@nameserver cat /var/log/daemon.log  /var/log/daemon.log.1 |
-sed -n 's/ to .*//;s/.*DHCPACK on //p' |
-sort -u |
-while read ip ; do
-	hst=$(host $ip | sed 's/.* //')
-	name=$(printf '%-20.20s' "$hst")
-	echo "$name  $ip"
-done |
-grep -v 'NXDOMAIN' | sort
-ssh root@nameserver cat /var/log/daemon.log  /var/log/daemon.log.1 |
-sed -n 's/ to .*//;s/.*DHCPACK on //p' |
-sort -u |
-while read ip ; do
-	hst=$(host $ip | sed 's/.* //')
-	name=$(printf '%-20.20s' "$hst")
-	echo "$name  $ip"
-done |
-grep  'NXDOMAIN' | sort
+if [ "$1" = "-htm" ] ; then
+	echo "<h2>DHCP</h2>"
+	date
+	echo "<table>"
+	ssh root@nameserver cat /var/log/daemon.log  /var/log/daemon.log.1 |
+	sed -n 's/ to .*//;s/.*DHCPACK on //p' |
+	sort -u |
+	while read ip ; do
+		hst=$(host $ip | sed 's/.* //')
+		name=$(printf '%-20.20s' "$hst")
+		echo "<tr><td>$hst </td><td>$ip</td></tr>"
+	done |
+	sort |
+	grep -v  'NXDOMAIN' 
 
+	ssh root@nameserver cat /var/log/daemon.log  /var/log/daemon.log.1 |
+	sed -n 's/ to .*//;s/.*DHCPACK on //p' |
+	sort -u |
+	while read ip ; do
+		hst=$(host $ip | sed 's/.* //')
+		name=$(printf '%-20.20s' "$hst")
+		echo "<tr><td>$hst </td><td>$ip</td></tr>"
+	done |
+	grep  'NXDOMAIN'
+	echo "</table>"
+else
+	echo "               DHCP HOSTS"
+	echo "-----------------------------------------"
+	ssh root@nameserver cat /var/log/daemon.log  /var/log/daemon.log.1 |
+	sed -n 's/ to .*//;s/.*DHCPACK on //p' |
+	sort -u |
+	while read ip ; do
+		hst=$(host $ip | sed 's/.* //')
+		name=$(printf '%-20.20s' "$hst")
+		echo "$name  $ip"
+	done |
+	grep -v 'NXDOMAIN' | sort
+	ssh root@nameserver cat /var/log/daemon.log  /var/log/daemon.log.1 |
+	sed -n 's/ to .*//;s/.*DHCPACK on //p' |
+	sort -u |
+	while read ip ; do
+		hst=$(host $ip | sed 's/.* //')
+		name=$(printf '%-20.20s' "$hst")
+		echo "$name  $ip"
+	done |
+	grep  'NXDOMAIN' | sort
+fi
 
